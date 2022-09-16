94c50d3dbafbbe8f8dded1e5f9c6ae3035290f41
Omar Faruk <omarfaruk2y2@gmail.com>
2022-09-16 08:01:37 +0600

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navContainer'>
+            <span className='logo'>React</span>
+            <div className='navItem'>
+                <button className='navButton'>Register</button>
+                <button className='navButton'>Log In</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default Home;
